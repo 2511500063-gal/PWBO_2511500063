680c0f1f1528c892863b6089f832b19fd572cd91
GALANG NURHAFIZD <2511500063@mahasiswa.atmaluhur.ac.id>
2026-09-22 17:02:37 +0700

    update

--- app/Core/App.php
+++ app/Core/App.php
@@ -1,9 +1,49 @@
 <?php
 
-class App
-{
+class App {
+    protected $controller = 'Home'; //controller default
+    protected $method = 'index'; //method default
+    protected $params = []; //parameter default
+
     public function __construct()
     {
-        echo 'OK!'; //memastikan jika class App sudah berhasil dipanggil
+        $url = $this->parseURL();
+
+        //controller
+        if(file_exists('../app/controllers/' . $url[0] . '.php')) { //cek
+            //cek dulu apakah file di dalam folder controllers
+            $this->controller = $url[0];
+            unset($url[0]); //hapus elemen array ke1
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        //method
+        if(isset($url[1])){
+            if(method_exists($this->controller, $url[1])){
+                $this->method = $url[1];
+                unset($url[1]); //hapus elemen array ke2
+            }
+        }
+
+        //params
+        if(!empty($url)){
+            $this->params = array_values($url);
+        }
+
+        //jalankan controller & method, serta kirimkan params jika ada
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    }
+
+    public function parseURL()
+    {
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL); //
+            //membersihkan url dari karakter aneh
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
--- public/img/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once '../app/init.php' //file init ini yang akan memanggil
-// semua file yang kita butuhkan, teknik ini dinamakan bootstrapping
-// (panggil satu file, dan file tsb akan memanggil seluruh aplikasi MVC)
-
-$app = new App //memanggil class App
